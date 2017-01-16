68cd25d15df271bbe90afd3a288280c7e81529a5
shak1864 <patelt19@wpsma.org>
2017-01-16 18:02:05 -0500

    :camel:  :100:  (Avi) Finally got code
    We finaly familierized ourselves with GRIP and have started to generate
    code based on WPI's instructions. We will work on flushing the code out
    as well as creating classes and commands to call the information stored
    in the network tables created by GRIP.

--- src/org/usfirst/frc/team1153/robot/Robot.java
+++ src/org/usfirst/frc/team1153/robot/Robot.java
@@ -1,16 +1,22 @@
 
 package org.usfirst.frc.team1153.robot;
 
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 import org.usfirst.frc.team1153.robot.commands.ExampleCommand;
 import org.usfirst.frc.team1153.robot.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.vision.VisionThread;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,20 +29,43 @@ public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
+	private static final int IMG_WIDTH = 320;
+	private static final int IMG_HEIGHT = 240; 
+	
+	private VisionThread visionThread;
+	private double centerX = 0.0; 
+	private RobotDrive drive; 
+	
+	private final Object imgLock = new Object();  
 
     Command autonomousCommand;
     SendableChooser chooser;
+    NetworkTable table;
 
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+  
 		oi = new OI();
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
+        double[] defaultValue = new double[0];
+        while (true) {
+        	double[] areas = table.getNumberArray("area", defaultValue);
+        	System.out.print("areas: ");
+        	for (double area : areas) {
+        		System.out.print(area + " ");
+        	}
+        	System.out.println();
+        	Timer.delay(1);
+        }
+        
+        
+        
     }
 	
 	/**
