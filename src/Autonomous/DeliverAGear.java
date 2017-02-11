e35fa24435110b1786b5afd7120b0173f1a8609b
Aayush, Brigham & Avi <mathura20@wpsma.org>
2017-02-11 17:58:55 -0500

    Integrated gear subsystem command into autonomous
    After merging the Timer and the Drive, we added the gear subsystem
    command to release the gear into our overall autonomous DeliverAGear
    code.

--- src/Autonomous/DeliverAGear.java
+++ src/Autonomous/DeliverAGear.java
@@ -2,6 +2,7 @@ package Autonomous;
 
 import com.walpole.frc.team.robot.commands.DriveBackwardsWithEncoder;
 import com.walpole.frc.team.robot.commands.DriveForwardWithEncoder;
+import com.walpole.frc.team.robot.commands.ReleaseGearCommand;
 import com.walpole.frc.team.robot.commands.TurnWithGyroCommand;
 import com.walpole.frc.team.robot.commands.WaitInbetweenCommandsCommand;
 
@@ -20,6 +21,7 @@ public class DeliverAGear extends CommandGroup {
 	// TODO Auto-generated constructor stub
 
 	addSequential(new DriveForwardWithEncoder(inchesToAirShip));
+	addSequential(new ReleaseGearCommand());
 	addSequential(new WaitInbetweenCommandsCommand(secondsToDrive));
 	addSequential(new DriveBackwardsWithEncoder(inchesBack));
 	addSequential(new TurnWithGyroCommand(degreesToTurn));
