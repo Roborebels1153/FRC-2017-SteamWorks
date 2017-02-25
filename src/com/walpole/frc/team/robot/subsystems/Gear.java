f0b2ae88d7e3fe01a95114fb1acda7d9e12462b1
Aayush, Brigham & Avi <mathura20@wpsma.org>
2017-02-25 14:53:23 -0500

    Switched from DoubleSolenoid to Solenoid

--- src/com/walpole/frc/team/robot/subsystems/Gear.java
+++ src/com/walpole/frc/team/robot/subsystems/Gear.java
@@ -2,20 +2,20 @@ package com.walpole.frc.team.robot.subsystems;
 
 import com.walpole.frc.team.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Gear extends Subsystem {
 	
-	private DoubleSolenoid gearSolenoidRetainer;
-	private DoubleSolenoid gearPusherSolenoid;
+	private Solenoid gearSolenoidRetainer;
+	private Solenoid gearPusherSolenoid;
 	
 	public Gear() {
 		
-		gearSolenoidRetainer = new DoubleSolenoid(RobotMap.GEAR_SOLENOID_A, RobotMap.GEAR_SOLENOID_B);
+		gearSolenoidRetainer = new Solenoid(RobotMap.GEAR_SOLENOID_A, RobotMap.GEAR_SOLENOID_B);
 		
 		
-		gearPusherSolenoid = new DoubleSolenoid(RobotMap.GEAR_PUSHER_SOLENOID_A, RobotMap.GEAR_PUSHER_SOLENOID_B);
+		gearPusherSolenoid = new Solenoid(RobotMap.GEAR_PUSHER_SOLENOID_A, RobotMap.GEAR_PUSHER_SOLENOID_B);
 		
 //		init();
 	}
@@ -29,26 +29,26 @@ public class Gear extends Subsystem {
 	
 	public void retractGearRetainer() {
 		
-		gearSolenoidRetainer.set(DoubleSolenoid.Value.kReverse);
+		gearSolenoidRetainer.set(false);
 		
 	}
 	
 	public void keepGear() {
 		
-		gearSolenoidRetainer.set(DoubleSolenoid.Value.kForward);
+		gearSolenoidRetainer.set(true);
 		
 		
 	}
 
 	public void retractGearPusher() {
 		
-		gearPusherSolenoid.set(DoubleSolenoid.Value.kReverse);
+		gearPusherSolenoid.set(false);
 		
 	}
 	
 	public void fireGearPusher() {
 		
-		gearPusherSolenoid.set(DoubleSolenoid.Value.kForward);
+		gearPusherSolenoid.set(true);
 	}
 
 	@Override
