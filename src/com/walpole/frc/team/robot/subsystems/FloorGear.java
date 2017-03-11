79e024a2505b4ea0a6bce22931934894fa8b5136
Brigham B <byerlybrigham@gmail.com>
2017-03-11 18:18:03 -0500

    Temporarily removed encoder code :golf:

--- src/com/walpole/frc/team/robot/subsystems/FloorGear.java
+++ src/com/walpole/frc/team/robot/subsystems/FloorGear.java
@@ -2,12 +2,8 @@ package com.walpole.frc.team.robot.subsystems;
 
 import com.walpole.frc.team.robot.Constants;
 import com.walpole.frc.team.robot.RobotMap;
-import com.walpole.frc.team.robot.lib.DualPIDOutput;
 
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -15,29 +11,29 @@ public class FloorGear extends Subsystem {
 	
 	private Victor gearMotorLeft;
 	private Victor gearMotorRight;
-	private DualPIDOutput dualPIDOutput;
+	//private DualPIDOutput dualPIDOutput;
 	
-	private Encoder gearEncoder;
+	//private Encoder gearEncoder;
 	
-	private PIDController gearPID;
+	//private PIDController gearPID;
 
 	public FloorGear() {
 		gearMotorLeft = new Victor(RobotMap.GEAR_MOTOR_LEFT);
 		gearMotorRight = new Victor(RobotMap.GEAR_MOTOR_RIGHT);
-		dualPIDOutput = new DualPIDOutput(gearMotorLeft, gearMotorRight);
+		//dualPIDOutput = new DualPIDOutput(gearMotorLeft, gearMotorRight);
 		
-		gearEncoder = new Encoder(RobotMap.GEAR_ENCODER_A, RobotMap.GEAR_ENCODER_B, false, EncodingType.k4X);
+		//gearEncoder = new Encoder(RobotMap.GEAR_ENCODER_A, RobotMap.GEAR_ENCODER_B, false, EncodingType.k4X);
 		
-		gearPID = new PIDController(0.2, 0, 0, gearEncoder, dualPIDOutput);
+		//gearPID = new PIDController(0.2, 0, 0, gearEncoder, dualPIDOutput);
 	}
 	
-	public void disableGearPID() {
+	/*public void disableGearPID() {
 		gearPID.disable();
-	}
+	}*/
 	
-	public void enableGearPID() {
+	/*public void enableGearPID() {
 		gearPID.enable();
-	}
+	}*/
 	
 	public void gear(Joystick joystick) {
 		double speed = Constants.FLOOR_GEAR_COLLECTOR_SPEED * joystick.getRawAxis(RobotMap.JOYSTICK_LEFT_Y);
